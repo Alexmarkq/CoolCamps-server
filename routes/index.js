5c699585baf9146d96438aa18149076b9541b564
Alex Marquez <alexis.marquez@icloud.com>
2022-12-06 17:33:56 +0100

    rent context

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,9 @@ module.exports = app => {
     app.use("/api/auth", authRoutes)
 
     // const postRoutes = require("./post.routes")
-    // app.use("/api/post", postRoutes)
+    // app.use("/api/post", postRoutes) 
+
+    const uploadRoutes = require("./upload.routes")
+    app.use("/api/upload", uploadRoutes)
 
 }
