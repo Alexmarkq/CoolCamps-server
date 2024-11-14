5ee59296c6ee1a36443ee77ca0fce3046e11aeca
Alexis Márquez <115575909+Alexmarkq@users.noreply.github.com>
2024-11-14 19:24:36 +0100

    app.use

--- config/index.js
+++ config/index.js
@@ -29,6 +29,14 @@ module.exports = (app) => {
     })
   );
 
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", FRONTEND_URL);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+  });
+
   // In development environment the app logs
   app.use(logger("dev"));
 
