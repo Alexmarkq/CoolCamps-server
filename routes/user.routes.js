d8e19b84fe0194585ccab50f87cbd56f3a8eefe5
Alexmarkq <alexis.marquez@icloud.com>
2025-01-23 21:29:08 +0100

    Consolidate imports in user routes for improved readability

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router()
-const { userDelete } = require('../controllers/user.controllers')
-const { getUser } = require('../controllers/user.controllers')
+const { userDelete, getUser } = require('../controllers/user.controllers')
 
 router.get('/:user_id', getUser)
 router.delete('/delete/:user_id', userDelete)
